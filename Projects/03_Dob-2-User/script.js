86392de18d78dc8312130b7531c4b5789ffe4e6c
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-12-16 21:02:08 +0530

    date dob

--- Projects/03_Dob-2-User/script.js
+++ Projects/03_Dob-2-User/script.js
@@ -1,23 +1,20 @@
 const form = document.querySelector('form')
 const user1Input = document.querySelector('input#user-1')
 const user2Input = document.querySelector('input#user-2')
-const p1 = document.querySelector('.p1')
+const p = document.querySelector('.p1')
 const p2 = document.querySelector('.p2')
 
 form.addEventListener('submit', (e) => {
   e.preventDefault() // prevents the page from reloading
-  const user1Date = parseDateString(user1Input.value)
-  const user2Date = parseDateString(user2Input.value)
+  const user1AgeInMs = Date.now() - parseDateString(user1Input.value).getTime()
+  const user2AgeInMs = Date.now() - parseDateString(user2Input.value).getTime()
 
-  if (user1Date > user2Date) {
-    p1.innerText = 'User 1 is older than User 2'
-    p2.innerText = `The difference is ${formatDateDifference(user1Date, user2Date)}`
-  } else if (user1Date < user2Date) {
-    p1.innerText = 'User 2 is older than User 1'
-    p2.innerText = `The difference is ${formatDateDifference(user2Date, user1Date)}`
+  if (user1AgeInMs > user2AgeInMs) {
+    p.innerText = 'User 1 is older than user 2'
+  } else if (user1AgeInMs < user2AgeInMs) {
+    p.innerText = 'User 2 is older than user 1'
   } else {
-    p1.innerText = 'Both are of the same age'
-    p2.innerText = ''
+    p.innerText = 'Both are of same age'
   }
 })
 
@@ -26,32 +23,4 @@ function parseDateString(dobString) {
   const [day, month, year] = dateString.split('/').map((el) => Number(el))
   const [hour, minute] = timeString.split(':').map((el) => Number(el))
   return new Date(year, month - 1, day, hour, minute)
-}
-
-function formatDateDifference(olderDate, youngerDate) {
-  let years = olderDate.getFullYear() - youngerDate.getFullYear()
-  let months = olderDate.getMonth() - youngerDate.getMonth()
-  let days = olderDate.getDate() - youngerDate.getDate()
-  let hours = olderDate.getHours() - youngerDate.getHours()
-  let minutes = olderDate.getMinutes() - youngerDate.getMinutes()
-
-  if (minutes < 0) {
-    minutes += 60
-    hours--
-  }
-  if (hours < 0) {
-    hours += 24
-    days--
-  }
-  if (days < 0) {
-    const previousMonth = new Date(olderDate.getFullYear(), olderDate.getMonth(), 0)
-    days += previousMonth.getDate()
-    months--
-  }
-  if (months < 0) {
-    months += 12
-    years--
-  }
-
-  return `${years} years, ${months} months, ${days} days, ${hours} hours, and ${minutes} minutes.`
 }
