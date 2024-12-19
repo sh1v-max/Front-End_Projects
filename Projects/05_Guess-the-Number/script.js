27fb3f9e8ed195d36eddb2b0514483dd1be74a0b
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-12-19 21:08:54 +0530

    done

--- Projects/05_Guess-the-Number/script.js
+++ Projects/05_Guess-the-Number/script.js
@@ -1,30 +1,47 @@
-const userInput = document.querySelector('.user-input')
-const form = document.querySelector('form')
-const result = document.querySelector('.result')
-const allGuesses = document.querySelector('.all-guesses')
-const submitBtn = document.querySelector('.submit-btn')
-const startGameBtn = document.querySelector('.start-game')
+;(function () {
+  const userInput = document.querySelector('.user-input')
+  const form = document.querySelector('form')
+  const result = document.querySelector('.result')
+  const allGuesses = document.querySelector('.all-guesses')
+  const submitBtn = document.querySelector('.submit-btn')
+  const startGameBtn = document.querySelector('.start-game')
 
-const allGuessesArray = []
-let randomNumber = Math.round(Math.random() * 100)
+  let allGuessesArray = []
+  let randomNumber = Math.round(Math.random() * 100)
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault() // to stop page from reloading
-  const userInputValue = parseInt(userInput.value)
-  // console.log(userInputValue)
-  // console.log(typeof(userInputValue))// string
-  // console.log(randomNumber)
-  if(userInputValue < randomNumber){
-    result.innerText = 'Too low!'
-  }else if (userInputValue > randomNumber){
-    result.innerText = 'Too high!'
-  }else {
-    result.innerText = 'Congrats... you got it right!!!'
-    startGameBtn.disabled = false 
-    submitBtn.disabled = true
-  }
-  // console.log('submitted')
-  allGuessesArray.push(userInputValue)
-  allGuesses.innerText = 'Your Guesses' + allGuessesArray.join(', ')
-  form.reset()
-})
+  form.addEventListener('submit', (e) => {
+    e.preventDefault() // to stop page from reloading
+    const userInputValue = parseInt(userInput.value)
+    // console.log(userInputValue)
+    // console.log(typeof(userInputValue))// string
+    // console.log(randomNumber)
+    if (userInputValue < randomNumber) {
+      result.innerText = 'Too low!'
+    } else if (userInputValue > randomNumber) {
+      result.innerText = 'Too high!'
+    } else {
+      result.innerText = 'Congrats... you got it right!!!'
+      startGameBtn.disabled = false
+      submitBtn.disabled = true
+    }
+    // console.log('submitted')
+    allGuessesArray.push(userInputValue)
+    allGuesses.innerText = 'Your Guesses' + allGuessesArray.join(', ')
+    form.reset()
+  })
+
+  startGameBtn.addEventListener('click', (e) => {
+    result.innerText = ''
+    allGuesses.innerText = ''
+    startGameBtn.disabled = true
+    submitBtn.disabled = false
+    allGuessesArray = []
+    randomNumber = Math.round(Math.random() * 100)
+  })
+})()
+
+//? why i am using iife?
+// https://youtu.be/L1Da3GWEGpY?list=PLfEr2kn3s-bpqqk5fI9p1DJ2WhYTxfLhw&t=2480
+
+//? iife is used to avoid global scope pollution
+// (function()()) // this is iife
