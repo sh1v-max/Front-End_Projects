3ea913ef0780c70557a94649d1bc4d3e8fecbe23
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-12-22 21:32:14 +0530

    don

--- Projects/10_Popup/script.js
+++ Projects/10_Popup/script.js
@@ -1,11 +1,17 @@
 const button = document.querySelector('button');
-const popup = document.querySelector('.popup');
 const closeIcon = document.querySelector('.close-icon');
+const popup = document.querySelector('.popup-container');
+const overlay = document.querySelector('.overlay');
 
+0.
 button.addEventListener('click', () => {
-  popup.classList.add('open')
+  popup.classList.add('popup-open')
 })
 
 closeIcon.addEventListener('click', () => {
-  popup.classList.remove('open')
+  popup.classList.remove('popup-open')
+})
+
+overlay.addEventListener('click', () => {
+  popup.classList.remove('popup-open')
 })
