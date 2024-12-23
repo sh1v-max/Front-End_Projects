8d926b6010186cb42d174b7b9de89163a60bd009
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-12-23 21:35:15 +0530

    auto typer

--- Projects/11_Auto-Typing-Effect/script.js
+++ Projects/11_Auto-Typing-Effect/script.js
@@ -1,39 +1,57 @@
-const span = document.querySelector('span');
-const wordsList = ['Developer.', 'Designer.', 'Freelancer.', 'Photographer.', 'Traveler.'];
+const span = document.querySelector('h1 span')
+const span2 = document.querySelector('h2 span')
+const wordsList = [
+  'Developer.',
+  'Designer.',
+  'Freelancer.',
+  'Photographer.',
+  'Traveler.',
+]
+const wordsList2 = [
+  'Play-Games.',
+  'Cook.',
+  'Read-Books.',
+  'Photograph.',
+  'Travel.',
+]
 
-let wordIndex = 0
-let characterIndex = 0;
-let skipUpdate = 0
-let reverseType = false
+function autoType(wordsList, element) {
+  let wordIndex = 0
+  let characterIndex = 0
+  let skipUpdate = 0
+  let reverseType = false
 
-const intervalId = setInterval(() => {
-  if(skipUpdate){
-    skipUpdate--
-    return
-  }
-  
-  if (!reverseType){
-    skipUpdate = 2
-    span.innerText = span.innerText + wordsList[wordIndex][characterIndex]
-    characterIndex++
-  }else {
-    span.innerText = span.innerText.slice(0, span.innerText.length - 1)
-    characterIndex--
-  }
+  const intervalId = setInterval(() => {
+    if (skipUpdate) {
+      skipUpdate--
+      return
+    }
 
-  if(characterIndex === wordsList[wordIndex].length){
-    skipUpdate = 15
-    reverseType = true
-  }
+    if (!reverseType) {
+      skipUpdate = 2
+      element.innerText = element.innerText + wordsList[wordIndex][characterIndex]
+      characterIndex++
+    } else {
+      element.innerText = element.innerText.slice(0, element.innerText.length - 1)
+      characterIndex--
+    }
 
-  if (span.innerText.length === 0 && reverseType){
-    reverseType = false
-    characterIndex = 0
-    wordIndex++
-  }
+    if (characterIndex === wordsList[wordIndex].length) {
+      skipUpdate = 15
+      reverseType = true
+    }
 
-  if(wordIndex === wordsList.length){
-    wordIndex = 0
-  }
-  
-}, 50)
+    if (element.innerText.length === 0 && reverseType) {
+      reverseType = false
+      characterIndex = 0
+      wordIndex++
+    }
+
+    if (wordIndex === wordsList.length) {
+      wordIndex = 0
+    }
+  }, 50)
+}
+
+autoType(wordsList, span)
+autoType(wordsList2, span2)
