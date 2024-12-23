855555a6761964fceaf1126c76c0bee40aa71f16
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-12-23 20:27:15 +0530

    auto typing

--- Projects/11_Auto-Typing-Effect/script.js
+++ Projects/11_Auto-Typing-Effect/script.js
@@ -1,25 +1,39 @@
 const span = document.querySelector('span');
+const wordsList = ['Developer.', 'Designer.', 'Freelancer.', 'Photographer.', 'Traveler.'];
 
-const wordsList = ['Developer', 'Designer', 'Freelancer', 'Photographer', 'Traveler'];
-
-const word = 'Developer'
-
+let wordIndex = 0
 let characterIndex = 0;
+let skipUpdate = 0
 let reverseType = false
 
 const intervalId = setInterval(() => {
+  if(skipUpdate){
+    skipUpdate--
+    return
+  }
+  
   if (!reverseType){
-    span.innerText = span.innerText + word[characterIndex]
+    skipUpdate = 2
+    span.innerText = span.innerText + wordsList[wordIndex][characterIndex]
     characterIndex++
   }else {
     span.innerText = span.innerText.slice(0, span.innerText.length - 1)
+    characterIndex--
+  }
+
+  if(characterIndex === wordsList[wordIndex].length){
+    skipUpdate = 15
+    reverseType = true
   }
 
   if (span.innerText.length === 0 && reverseType){
     reverseType = false
     characterIndex = 0
+    wordIndex++
   }
-  if(characterIndex === word.length){
-    reverseType = true
+
+  if(wordIndex === wordsList.length){
+    wordIndex = 0
   }
-}, 200)
+  
+}, 50)
