43d962c5dd68a0c5cc41c84502b3d61d6315a272
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-12-25 20:43:14 +0530

    meme genrator done

--- Projects/13_Meme-Generator/script.js
+++ Projects/13_Meme-Generator/script.js
@@ -1,10 +1,22 @@
 const generateBtn = document.querySelector('.generate-button');
+const memeTitle = document.querySelector('.meme-title');
+const memeImage = document.querySelector('.meme-image');
+const authorOutput = document.querySelector('.author');
 
-generateBtn.addEventListener('click', () => {
+function getMeme() {
   fetch('https://meme-api.com/gimme/wholesomememes')
   .then((res => res.json()))
   .then((data) => {
     const {author, title, url} = data;
+    memeTitle.innerText = title;
+    memeImage.src = url;
+    authorOutput.innerText = `Meme By: ${author}`;
     console.log(author, title, url);
   })
+}
+
+getMeme()
+
+generateBtn.addEventListener('click', () => {
+  getMeme()
 })
