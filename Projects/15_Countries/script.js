1d1399577eb199f45620c3da83614f64538c59b3
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-12-27 16:40:27 +0530

    api fetched

--- Projects/15_Countries/script.js
+++ Projects/15_Countries/script.js
@@ -1,26 +1,21 @@
-// fetch('https://restcountries.com/v3.1/all')
-// .then((res) => res.json())
-// .then((data) => {
-//     data.forEach((country) => {
-//         console.log(country)
-//     })
-// })
+const countriesContainer = document.querySelector('.countries-container')
 
-const countryCard = document.createElement('a')
-countryCard.classList.add('countryCard')
-
-// const cardImg = document.createElement('img')
-// cardImg.src = 'https://flagcdn.com/in.svg'
-// countryCard.append(cardImg)
-
-const cardHTML = `
-<img src="https://flagcdn.com/in.svg" alt="flag">
-<div class="card-text">
-  <h3 class="card-title">India</h3>
-  <p><b>Population: </b>1,411,978,324</p>
-  <p><b>Region: </b>Asia</p>
-  <p><b>Capital: </b>New Delhi</p>
-</div>
-`
-
-console.log(countryCard)
+fetch('https://restcountries.com/v3.1/all')
+.then((res) => res.json())
+.then((data) => {
+    data.forEach((country) => {
+      console.log(country)
+      const countryCard = document.createElement('a')
+      countryCard.classList.add('country-card')
+      countryCard.innerHTML =  `
+      <img src="${country.flags.svg}" alt="flag">
+      <div class="card-text">
+        <h3 class="card-title">${country.name.common}</h3>
+        <p><b>Population: </b>${country.population.toLocaleString()}</p>
+        <p><b>Region: </b>${country.region}</p>
+        <p><b>Capital: </b>${country.capital}</p>
+      </div>
+    `
+      countriesContainer.appendChild(countryCard)
+    })
+})
