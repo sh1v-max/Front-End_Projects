1670b4a038796dd239c6e6ab70a434b668954b27
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-12-27 21:19:21 +0530

    countries other page

--- Projects/15_Countries/script.js
+++ Projects/15_Countries/script.js
@@ -9,7 +9,7 @@ fetch('https://restcountries.com/v3.1/all')
       countryCard.classList.add('country-card')
       countryCard.href = `/Projects/15_Countries/country.html?name=${country.name.common}`
       countryCard.innerHTML = `
-      <img src="${country.flags.svg}" alt="flag">
+      <img src="${country.flags.svg}" alt="${country.name.common} flag">
       <div class="card-text">
         <h3 class="card-title">${country.name.common}</h3>
         <p><b>Population: </b>${country.population.toLocaleString()}</p>
