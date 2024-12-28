003464e3353a9b1a15baff649e17292f24c98311
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-12-28 16:06:14 +0530

    border countries

--- Projects/15_Countries/country.js
+++ Projects/15_Countries/country.js
@@ -9,33 +9,63 @@ const capital = document.querySelector('.capital')
 const topLevelDomain = document.querySelector('.top-level-domain')
 const currencies = document.querySelector('.currencies')
 const languages = document.querySelector('.languages')
+const timeZone = document.querySelector('.time-zone')
+const mapLocation = document.querySelector('.map-location')
 
 fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
-.then((res) => res.json())
-.then(([country]) => {
-  // data[0] can also be written as [country], according to destructuring
-  // console.log(Object.values(country.name.nativeName)[0].common)
+  .then((res) => res.json())
+  .then(([country]) => {
+    // data[0] can also be written as [country], according to destructuring
+    // console.log(Object.values(country.name.nativeName)[0].common)
+    // console.log(country)
     flagImage.src = country.flags.svg
     countryNameH1.innerText = country.name.common
-    population.innerText = country.population.toLocaleString('en-IN')
+    population.innerText = country.population.toLocaleString()
     region.innerText = country.region
     topLevelDomain.innerText = country.tld.join(', ')
+    timeZone.innerText = country.timezones.join(', ')
+    mapLocation.href = country.maps.googleMaps
 
-  if(country.name.nativeName){
-    nativeName.innerText = Object.values(country.name.nativeName)[0].common
-  }else{
-    nativeName.innerText = country.name.common
-  }
+    if (country.name.nativeName) {
+      nativeName.innerText = Object.values(country.name.nativeName)[0].common
+    } else {
+      nativeName.innerText = country.name.common
+    }
 
-  if (country.capital) {
-    capital.innerText = country.capital?.[0]
-  }
+    if (country.capital) {
+      capital.innerText = country.capital?.[0]
+    } else {
+      capital.innerText = 'N/A'
+    }
 
-  if (country.subregion) {
-    subRegion.innerText = country.subregion
-  }
+    if (country.subregion) {
+      subRegion.innerText = country.subregion
+    } else {
+      subRegion.innerText = 'N/A'
+    }
 
-  if (country.languages) {
-    languages.innerText = Object.values(country.languages).join(', ')
-  }
-})
+    if (country.languages) {
+      languages.innerText = Object.values(country.languages).join(', ')
+    } else {
+      languages.innerText = 'N/A'
+    }
+
+    if (country.currencies) {
+      currencies.innerText = Object.values(country.currencies)
+        .map((currency) => currency.name)
+        .join(`, `)
+    } else {
+      currencies.innerText = 'N/A'
+    }
+
+    if (country.borders) {
+      country.borders.forEach((border) => {
+        console.log(border)
+        fetch(`https://restcountries.com/v3.1/alpha/${border}`)
+        .then((res) => res.json())
+        .then(([borderCountry]) => {
+          console.log(borderCountry)
+        })
+      })
+    }
+  })
