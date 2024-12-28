3af7d0603721b5f951e5b2eecf1b61ceee239d5c
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-12-28 19:04:00 +0530

    country

--- Projects/15_Countries/script.js
+++ Projects/15_Countries/script.js
@@ -4,34 +4,15 @@ const filterByRegion = document.querySelector('.filter-by-region')
 fetch('https://restcountries.com/v3.1/all')
   .then((res) => res.json())
   .then((data) => {
-    countriesContainer.innerHTML = ''
-
-    data.forEach((country) => {
-      // console.log(country.name.common)
-
-      const countryCard = document.createElement('a')
-      countryCard.classList.add('country-card')
-      // countryCard.href = `/country.html?name=${country.name.common}`
-      countryCard.href = `/Projects/15_Countries/country.html?name=${country.name.common}`
-      countryCard.innerHTML = `
-      <img src="${country.flags.svg}" alt="${country.name.common} flag">
-      <div class="card-text">
-        <h3 class="card-title">${country.name.common}</h3>
-        <p><b>Population: </b>${country.population.toLocaleString()}</p>
-        <p><b>Region: </b>${country.region}</p>
-        <p><b>Capital: </b>${country.capital?.[0]}</p>
-      </div>
-    `
-      countriesContainer.appendChild(countryCard)
-    })
+    renderCountries(data)
   })
 
 filterByRegion.addEventListener('change', (e) => {
-  // console.log(e.target.value)
-  // console.log(filterByRegion.value)
   fetch(`https://restcountries.com/v3.1/region/${filterByRegion.value}`)
     .then((res) => res.json())
-    .then((data) => {})
+    .then((data) => {
+      renderCountries(data)
+    })
 })
 
 function renderCountries(data) {
