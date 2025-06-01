008fb8e970229b5d4fa51cdb08b677dbfcc811d4
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-06-01 12:34:35 +0530

    lazy loading

--- Projects/Practice/starter-code-laxu-loading/src/components/About.jsx
+++ Projects/Practice/starter-code-laxu-loading/src/components/About.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { todos } from '../data'
 
 export default function About() {
+  const [todosList, setTodosList] = useState([])
   return (
-    <h1 className='text-xl'>We are ProCodrrs</h1>
+    <>
+      <h1 className="text-xl">We are ProCodrrs</h1>
+      <button onClick={() => {
+        import('../data').then((module) => {
+          setTodosList(module.todos)
+        })
+      }}>load data</button>
+      <ul>
+        {todosList.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+    </>
   )
 }
