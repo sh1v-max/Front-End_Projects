6bc48d005e0104c57b0208a5bb9d707b1c45ef8e
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-06-01 11:24:55 +0530

    lazy loading

--- Projects/Practice/starter-code-laxu-loading/src/App.jsx
+++ Projects/Practice/starter-code-laxu-loading/src/App.jsx
@@ -1,13 +1,24 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header'
+import { Suspense } from 'react'
 
 function App() {
   return (
     <>
       <Header />
-      <main className="py-4 px-4 md:px-8">
-        <Outlet />
+      <main className="px-4 py-4 md:px-8">
+        <Suspense
+          fallback={
+            <div className="flex min-h-screen items-center justify-center bg-gray-100">
+              <div className="animate-pulse text-lg font-semibold text-gray-700">
+                Loading...
+              </div>
+            </div>
+          }
+        >
+          <Outlet />
+        </Suspense>
       </main>
     </>
   )
--- Projects/Practice/starter-code-laxu-loading/src/main.jsx
+++ Projects/Practice/starter-code-laxu-loading/src/main.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { lazy } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Home from './components/Home.jsx'
-import About from './components/About.jsx'
-import Contact from './components/Contact.jsx'
+// import Home from './components/Home.jsx'
+// import About from './components/About.jsx'
+// import Contact from './components/Contact.jsx'
+
+const Home = lazy(() => wait(1000).then(() => import('./components/Home')))
+const About = lazy(() => wait(1000).then(() => import('./components/About')))
+const Contact = lazy(() => wait(1000).then(() => import('./components/Contact')))
+
+const wait = (time) => {
+  return new Promise(resolve => {
+    setTimeout(() =>{
+      resolve()
+    }, time)
+  })
+}
 
 const router = createBrowserRouter([
   {
